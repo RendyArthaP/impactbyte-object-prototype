7f1d4267ec0f793310ed13c607eb7a82421bce06
Rendy Artha <rendyartha312@gmail.com>
2021-02-25 22:42:45 +0700

    soal 2

--- soal2/index.js
+++ soal2/index.js
@@ -1,1 +1,11 @@
-console.log('object')
+Number.prototype.isPrima = function() {
+  for(let i = 2; i < this; i++) {
+    if(this % i == 0) {
+      return false
+    }
+  }
+  return true
+}
+
+let number = 9
+console.log(number.isPrima())
